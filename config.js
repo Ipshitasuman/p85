dd95cb2f3f339a010a53378c8f672ba5e748c8cb
Ipshitasuman <ipshitasuman123@gmail.com>
2022-04-15 14:22:35 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD7bMwKFuGB9OEUG9JEzAwnwJl_GftcCGs",
+  authDomain: "spectagram-df8f2.firebaseapp.com",
+  projectId: "spectagram-df8f2",
+  storageBucket: "spectagram-df8f2.appspot.com",
+  messagingSenderId: "906381730145",
+  appId: "1:906381730145:web:9eeee6906c5f4de16ba520"
+};
